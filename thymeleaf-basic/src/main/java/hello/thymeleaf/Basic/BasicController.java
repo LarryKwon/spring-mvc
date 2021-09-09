9ac7bf20309c4e06c2a4ea4abbe24af279fbd778
larrykwon <koallarry11@kaist.ac.kr>
2021-09-09 23:35:19 +0900

    first commit

---
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/Basic/BasicController.java
@@ -0,0 +1,84 @@
+package hello.thymeleaf.Basic;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/basic")
+public class BasicController {
+
+    @GetMapping("/text-basic")
+    public String textBasic(Model model){
+        model.addAttribute("data","Hello Spring!");
+        return "basic/text-basic";
+    }
+
+    @GetMapping("/text-unescaped")
+    public String textUnescaped(Model model){
+        model.addAttribute("data","Hello <b>Spring!</b>");
+        return "basic/text-unescape";
+    }
+
+    @GetMapping("/variable")
+    public String variable(Model model){
+        User userA = new User("UserA",10);
+        User userB = new User("UserB",20);
+
+        List<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+
+        Map<String, User> userMap = new HashMap<>();
+        userMap.put("userA", userA);
+        userMap.put("userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("users",list);
+        model.addAttribute("userMap",userMap);
+
+        return "basic/variable";
+    }
+
+    @Data
+    public class User{
+        String name;
+        int age;
+
+        public User(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+    }
+
+    @GetMapping("/basic-objects")
+    public String basicObjects(HttpSession session){
+        session.setAttribute("sessionData","Hello Session");
+        return "basic/basic-objects";
+    }
+
+    @Component("HelloBean")
+    public class HelloBean{
+        public String hello(String data){
+            return "Hello" + data;
+        }
+    }
+
+    @GetMapping("/link")
+    public String link(Model model){
+        model.addAttribute("param1","data1");
+        model.addAttribute("param2","data2");
+
+        return "basic/link";
+    }
+
+}
